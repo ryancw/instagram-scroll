c2382171713864475bfa84343daf3b9e6c6e878a
ryancw <ryancwilliams9@gmail.com>
2015-02-11 15:31:09 -0500

    Fix variable scope

--- instagram-scroll.js
+++ instagram-scroll.js
@@ -8,23 +8,23 @@ function InstagramScroll(opts){
   if (!(this.opts.tagName || this.opts.clientID || this.opts.imageContainer)) {
     throw "Error: tagName, clientID, and imageContainer must be passed to InstagramScroll.";
   }
-  this.tagName = this.opts.tagName;
-  this.clientID = this.opts.clientID;
-  this.imageContainer = this.opts.imageContainer;
-  this.includeCaption = this.opts.includeCaption;
-  this.includeUsername = this.opts.includeUsername;
-  this.imageQuality = this.opts.imageQuality || 'standard_resolution';
-  this.imageSize = this.opts.imageSize || 200;
+  var tagName = this.opts.tagName;
+  var clientID = this.opts.clientID;
+  var imageContainer = this.opts.imageContainer;
+  var includeCaption = this.opts.includeCaption;
+  var includeUsername = this.opts.includeUsername;
+  var imageQuality = this.opts.imageQuality || 'standard_resolution';
+  var imageSize = this.opts.imageSize || 200;
    // options: low_resolution (300 x 300)
    // standard_resolution (600 x 600)
    // thumbnail (150 x 150)
-  this.scrollDistance = this.opts.scrollDistance || 350;
+  var scrollDistance = this.opts.scrollDistance || 350;
 
 
   var url = 'https://api.instagram.com/v1/tags/'
-            + this.tagName
+            + tagName
             + '/media/recent?client_id='
-            + this.clientID;
+            + clientID;
 
   function _loadResults(){
     $.ajax({
@@ -39,28 +39,31 @@ function InstagramScroll(opts){
   function _processData(response){
     if(response != null){
       var ul = $('<ul/>');
-      ul.attr({'class': this.tagName});
+      ul.attr({'class': tagName});
 
       $(response.data).each(function(index,obj){
         if(index == 20)
           return response.pagination.next_url;
 
         var link = $('<a/>'), image = $('<img/>'), li = $('<li/>');
-        image.attr({'src': obj.images[this.imageQuality].url,
-          'width': this.imageSize, 'height': this.imageSize});
+        var imgSrc = obj.images[imageQuality];
+        imgSrc = imgSrc.url;
+        image.attr({'src': imgSrc,
+                    'width': imageSize,
+                    'height': imageSize});
         link.attr('href',obj.link);
         image.appendTo(link);
         link.appendTo(li);
-        if(this.includeUsername){
+        if(includeUsername){
           $('<div class="username">'+obj.caption.from.username+'</div>').appendTo(li);
         }
-        if(this.includeCaption){
+        if(includeCaption){
           $('<div class="caption">'+obj.caption.text+'</div>').appendTo(li);
         }
         ul.append(li);
       });
 
-      $(this.imageContainer).append(ul);
+      $(imageContainer).append(ul);
       url = response.pagination.next_url;
     }
   };
@@ -81,7 +84,7 @@ function InstagramScroll(opts){
 
   $(window).scroll(function() {
     if($(window).scrollTop() + $(window).height() >
-       $(document).height() - this.scrollDistance) {
+       $(document).height() - scrollDistance) {
       _loadNext();
     }
   });
